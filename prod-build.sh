52866533262aad44d47a1183d790400b31537bb4
Dylan Cutler <DCtheTall@users.noreply.github.com>
2017-07-13 01:37:58 -0400

    More deploy stuff

---
+++ prod-build.sh
@@ -0,0 +1,6 @@
+cp webgl-obj-loader.d.ts node_modules/webgl-obj-loader;
+cd node_modules/webgl-obj-loader;
+mv webgl-obj-loader.d.ts index.d.ts;
+cd ../..;
+webpack --progress;
+echo "Build Succeeded";
