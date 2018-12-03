7a83bf32b7572919b55ac2343b378dfcbe84fcaa
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-12-02 18:34:31 -0700

    basic setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const { join, resolve } = require('path');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(
+  express.static(
+    join(resolve('.'), 'public')));
+app.set('view engine', 'pug');
+app.set('views', join(resolve('.'), 'views'));
+app.get('/', (_, res) =>
+  res.render('index', { dev: process.env.NODE_ENV === 'development' }));
+
+module.exports = app;
